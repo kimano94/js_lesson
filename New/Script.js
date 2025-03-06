d8914986816b228333855eb82875fe06944428ad
Kibayu <159928105+Kibayu23@users.noreply.github.com>
2025-03-06 19:32:47 +0300

    Add files via upload

--- New/Script.js
+++ New/Script.js
@@ -1,5 +1,7 @@
 TrCount = 5;
 TdCount = 25;
+Row_id = 1;
+Col_id = 1;
 let Mode = "Normal";
 
 function NewTable() {
@@ -27,8 +29,10 @@ function NewTr() {
 function NewTd() {
   let input = document.getElementById("Col");
   let tr = document.getElementsByTagName("tr");
+  let Old_id = Col_id;
   TdCount += input.value;
   for (let row of tr) {
+    Col_id = Old_id;
     row.append(...AddTd(input.value));
   }
 }
@@ -37,6 +41,9 @@ function AddTr(Count) {
   let result = [];
   for (let i = 0; i <= Count - 1; i++) {
     let tr = document.createElement("tr");
+    tr.id = Row_id;
+    Row_id += 1;
+    Col_id = 1;
     tr.append(...AddTd(TdCount));
     result.push(tr);
   }
@@ -47,6 +54,8 @@ function AddTd(Count) {
   let result = [];
   for (let i = 0; i <= Count - 1; i++) {
     let td = document.createElement("td");
+    td.id = Col_id;
+    Col_id += 1;
     result.push(td);
   }
   return result;
@@ -58,15 +67,33 @@ function ChangeBg() {
   body.style.backgroundColor = bgColor;
 }
 
-function ChangeMode() {
-  let UI = document.getElementsByClassName("UI");
-  for (let UIElem of UI) {
-    UIElem.hidden = 1;
-    if (UIElem.id == "Change_Mode") {
-      UIElem.hidden = 0;
+function Search() {
+  console.log("1");
+  let tr = document.getElementsByTagName("tr");
+  let Ver = document.getElementById("Tr");
+  let td = document.getElementsByTagName("td");
+  let Hor = document.getElementById("Td");
+  for (Col in tr) {
+    if (Col.id == Ver.value) {
+      for (Row in td) {
+        if (Row.id == Hor.value) {
+          Row.backgroundColor == "#40484f";
+        }
+      }
     }
   }
+}
+
+function ChangeMode() {
+  let UI = document.getElementsByClassName("UI");
+  let Search = document.getElementsByClassName("Search");
   if (Mode == "Normal") {
+    for (let UIElem of UI) {
+      UIElem.hidden = 1;
+      if (UIElem.id == "Change_Mode") {
+        UIElem.hidden = 0;
+      }
+    }
     Mode = "Value";
     let td = document.getElementsByTagName("td");
     for (let cell of td) {
@@ -77,6 +104,26 @@ function ChangeMode() {
       cell.append(input);
     }
   } else if (Mode == "Value") {
+    for (let UIElem of Search) {
+      UIElem.hidden = 0;
+    }
+    Mode = "Search";
+    let td = document.getElementsByTagName("td");
+    for (let cell of td) {
+      let input = document.getElementById("Value");
+      cell.innerHTML = input.value;
+      input.remove();
+    }
+  } else if (Mode == "Search") {
+    for (let UIElem of UI) {
+      UIElem.hidden = 0;
+      if (UIElem.id == "Table_Button") {
+        UIElem.hidden = 1;
+      }
+    }
+    for (let UIElem of Search) {
+      UIElem.hidden = 1;
+    }
     Mode = "Normal";
     let td = document.getElementsByTagName("td");
     for (let cell of td) {
