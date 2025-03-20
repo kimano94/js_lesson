3008a2b6db55243c7fdb94dc5b90aa6a65bae65a
DenisSerge <159931428+DenisSerge@users.noreply.github.com>
2025-03-20 19:35:05 +0300

    Add files via upload

--- script.mjs
+++ script.mjs
@@ -17,7 +17,23 @@ import fs from 'fs'
 // 	console.log('при чтении файла возникла ошибка', err);
 // }
 
-fs.readFile('a.txt', 'utf8', function(err, ta){
-	fs.writeFile("a.txt", ta * ta, function(err){
-	})
-}) 
+// fs.readFile('a.txt', 'utf8', function(err, ta){
+// 	fs.writeFile("a.txt", ta * ta, function(err){
+// 	})
+// }) 
+// fs.promises.readFile("a.txt","utf8").then(a => {
+// 	b = String(a)
+// 	console.log(Number(b[0]) + Number(b[1]) + Number(b[2]))
+// } )
+
+// fs.readFile("a.txt","utf8", function(err, b){
+// 	console.log(b**2)
+// })
+// fs.readFile('a.txt', 'utf8', function(err, b) {
+// 	if (!err) {
+// 	console.log(b)
+// 	console.log('раньше файла')
+// } else {
+// 	console.log('Ошибка в чтении файла')
+// }
+// })
