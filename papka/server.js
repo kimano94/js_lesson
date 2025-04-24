b38864f1aaf1f03895408d57647cac2290d523b8
DenisSerge <159931428+DenisSerge@users.noreply.github.com>
2025-04-24 19:46:44 +0300

    Add files via upload

---
+++ papka/server.js
@@ -0,0 +1,24 @@
+const http = require("http")
+const fs = require("fs");
+http.createServer( async (request, response) => {
+    if( request.url == "/user"){
+
+        let body = ""
+        for await (const chunk of request) {
+            body += chunk;
+          }
+          let username = ""
+          let userage = 0
+          const params = body.split("&")
+          for(param of params){
+            const [paramName, paramValue] = param.split("=");
+            if(paramName === "username") username = paramValue;
+            if(paramName === "userage") userage = paramValue;
+        
+          }
+          response.end(`Your name: ${username}  Your Age: ${userage}`);
+        }
+        else{
+            fs.readFile("index.html", (_, data) => response.end(data));
+        }
+}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
