85f4364b5da796193f92bf57da03b93b075459b7
Kibayu <159928105+Kibayu23@users.noreply.github.com>
2025-04-24 19:38:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+TrCount = 5;
+TdCount = 25;
+Row_id = 1;
+Col_id = 1;
+let Mode = "Normal";
+
+function NewTable() {
+  let div = document.getElementById("Table_div");
+  let UI = document.getElementsByClassName("UI");
+  let table = document.createElement("table");
+  table.id = "Table";
+  table.append(...AddTr(TrCount));
+  div.append(table);
+  for (let UIElem of UI) {
+    if (UIElem.hidden != 1) {
+      UIElem.hidden = 1;
+    } else if (UIElem.hidden != 0) {
+      UIElem.hidden = 0;
+    }
+  }
+}
+
+function NewTr() {
+  let table = document.getElementById("Table");
+  let input = document.getElementById("Row");
+  TrCount += input.value;
+  table.append(...AddTr(input.value));
+}
+function NewTd() {
+  let input = document.getElementById("Col");
+  let tr = document.getElementsByTagName("tr");
+  let Old_id = Col_id;
+  TdCount += input.value;
+  for (let row of tr) {
+    Col_id = Old_id;
+    row.append(...AddTd(input.value));
+  }
+}
+
+function AddTr(Count) {
+  let result = [];
+  for (let i = 0; i <= Count - 1; i++) {
+    let tr = document.createElement("tr");
+    tr.id = Row_id;
+    Row_id += 1;
+    Col_id = 1;
+    tr.append(...AddTd(TdCount));
+    result.push(tr);
+  }
+  return result;
+}
+
+function AddTd(Count) {
+  let result = [];
+  for (let i = 0; i <= Count - 1; i++) {
+    let td = document.createElement("td");
+    td.id = Col_id;
+    Col_id += 1;
+    result.push(td);
+  }
+  return result;
+}
+
+function Search() {
+  let td = document.getElementsByTagName("td");
+  for (let cell of td) {
+    cell.style.backgroundColor = "#202428";
+  }
+  let Ver = document.getElementById("Tr");
+  let Hor = document.getElementById("Td");
+  let table = document.getElementById("Table");
+  for (tr of table.children) {
+    if (tr.id == Ver.value) {
+      for (td of tr.children) {
+        if (td.id == Hor.value) {
+          td.style.backgroundColor = "#40484f";
+        }
+      }
+    }
+  }
+}
+
+function ChangeMode() {
+  let UI = document.getElementsByClassName("UI");
+  let Search = document.getElementsByClassName("Search");
+  if (Mode == "Normal") {
+    for (let UIElem of UI) {
+      UIElem.hidden = 1;
+      if (UIElem.id == "Change_Mode") {
+        UIElem.hidden = 0;
+      }
+    }
+    Mode = "Value";
+    let td = document.getElementsByTagName("td");
+    for (let cell of td) {
+      let input = document.createElement("input");
+      input.value = cell.innerHTML;
+      cell.innerHTML = "";
+      input.id = "Value";
+      cell.append(input);
+    }
+  } else if (Mode == "Value") {
+    for (let UIElem of Search) {
+      UIElem.hidden = 0;
+    }
+    Mode = "Search";
+    let td = document.getElementsByTagName("td");
+    for (let cell of td) {
+      let input = document.getElementById("Value");
+      cell.innerHTML = input.value;
+      input.remove();
+    }
+  } else if (Mode == "Search") {
+    for (let UIElem of UI) {
+      UIElem.hidden = 0;
+      if (UIElem.id == "Table_Button") {
+        UIElem.hidden = 1;
+      }
+    }
+    for (let UIElem of Search) {
+      UIElem.hidden = 1;
+    }
+    Mode = "Normal";
+    let td = document.getElementsByTagName("td");
+    for (let cell of td) {
+      cell.style.backgroundColor = "#202428";
+    }
+  }
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+import http from "http";
+import fs from "fs";
+
+function getMimeType(path) {
+  let mimes = {
+    html: "text/html",
+    jpeg: "image/jpeg",
+    jpg: "image/jpeg",
+    png: "image/png",
+    svg: "image/svg+xml",
+    json: "application/json",
+    js: "application/javascript",
+    css: "text/css",
+    ico: "image/x-icon",
+    ttf: "font/ttf",
+  };
+
+  let exts = Object.keys(mimes);
+  let extReg = new RegExp("\\.(" + exts.join("|") + ")$");
+
+  let ext = path.match(extReg)[1];
+
+  if (ext) {
+    return mimes[ext];
+  } else {
+    return "text/plain";
+  }
+}
+
+http
+  .createServer(async (request, response) => {
+    if (request.url == "/") {
+      request.url = "/index.html";
+    }
+    let text;
+    let status;
+    let path = "root" + request.url;
+    console.log(path);
+    try {
+      status = 200;
+      text = await fs.promises.readFile(path, "utf8");
+    } catch (err) {
+      status = 404;
+      text = await fs.promises.readFile("root/404.html", "utf8");
+    }
+    response.writeHead(status, {
+      "Content-Type": getMimeType(path),
+    });
+    response.write(text);
+    response.end();
+  })
+  .listen(3000);
