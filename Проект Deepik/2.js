652c83f516d2e6876f8635737564391b25bac28f
DenisSerge <159931428+DenisSerge@users.noreply.github.com>
2025-05-20 19:34:58 +0300

    Add files via upload

--- Проект Deepik/2.js
+++ Проект Deepik/2.js
@@ -1,4 +1,18 @@
-const fs = require("fs");
+let currentPage = 1
+const loadMoreData = () => {
+setTimeout(() => {
+    for(let i=0; i<10; i++) {
+        document.getElementByld('content').innerHTML += `<div class="item">Элемент ${currentPage * 10 + i}</div>`
+    }
+    currentPage++
+}, 1000)
+}
+window.addEventListener("scroll", function() {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            console.log("Достигнута нижняя граница страницы");
+            loadMoreData();
+        }
+    });
 
-let a = document.getElementById("file").value
-fs.readFile(a, (_, data))
+    // Загрузка начальных данных
+    loadMoreData();
